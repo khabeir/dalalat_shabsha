c1b0fd5773540a45eaa94a2560fcd3b719bf9c34
Family Tree Developer <familytree@example.com>
2026-09-19 21:31:42 +0200

    Connect Flutter app to Supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: const String.fromEnvironment('SUPABASE_URL'),
+    anonKey: const String.fromEnvironment('SUPABASE_ANON_KEY'),
+  );
   runApp(const DalalatShabshaApp());
 }
 
