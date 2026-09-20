4c006eaad7957d7098f0975fad138167ec95c543
Family Tree Developer <familytree@example.com>
2026-09-20 18:41:35 +0200

    Allow browsing without login

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-import 'screens/auth_screen.dart';
 import 'screens/home_screen.dart';
 
 Future<void> main() async {
@@ -27,27 +26,7 @@ class DalalatShabshaApp extends StatelessWidget {
         useMaterial3: true,
       ),
       locale: const Locale('ar'),
-      home: const AuthGate(),
-    );
-  }
-}
-
-class AuthGate extends StatelessWidget {
-  const AuthGate({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<AuthState>(
-      stream: Supabase.instance.client.auth.onAuthStateChange,
-      builder: (context, snapshot) {
-        final session = Supabase.instance.client.auth.currentSession;
-
-        if (session != null) {
-          return const HomeScreen();
-        }
-
-        return const AuthScreen();
-      },
+      home: const HomeScreen(),
     );
   }
 }
