ce7f58a2da54c6586832058c33d89d53c6548bba
Family Tree Developer <familytree@example.com>
2026-09-20 19:11:36 +0200

    Fix auth screen import

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -1,4 +1,4 @@
-دimport 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class AuthScreen extends StatefulWidget {
